7cb0508832857b513eef5a1cddf52ddc503f190a
Antonio <antonio241710@gmail.com>
2021-10-14 18:11:58 +0200

    added NewsAPI - Obtener noticias del mundo

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/src/pages/tabs_page.dart';
+import 'package:news_app/src/services/news_service.dart';
 import 'package:news_app/src/theme/tema.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: miTema,
-      debugShowCheckedModeBanner: false,
-      home: TabsPage()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => new NewsService() ),
+      ],
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: miTema,
+          debugShowCheckedModeBanner: false,
+          home: TabsPage()
+      ),
     );
   }
 }
---
+++ lib/src/services/news_service.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/src/models/news_models.dart';
+
+class NewsService with ChangeNotifier {
+
+  List<Article> headlines = [];
+
+  NewsService() {
+    this.getTopHeadlines();
+  }
+
+  getTopHeadlines(){
+    print('Cargando headlines...');
+  }
+
+}
