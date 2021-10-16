9d45626e50834330a871ca20599b23310dcbb7b2
Antonio <antonio241710@gmail.com>
2021-10-16 13:08:59 +0200

    added Loading de los Headlines y KeepAlive

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -3,7 +3,13 @@ import 'package:news_app/src/services/news_service.dart';
 import 'package:news_app/src/widgets/lista_noticias.dart';
 import 'package:provider/provider.dart';
 
-class Tab1Page extends StatelessWidget {
+class Tab1Page extends StatefulWidget {
+
+  @override
+  State<Tab1Page> createState() => _Tab1PageState();
+}
+
+class _Tab1PageState extends State<Tab1Page> with AutomaticKeepAliveClientMixin {
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +17,14 @@ class Tab1Page extends StatelessWidget {
     final headlines = Provider.of<NewsService>(context).headlines;
 
     return Scaffold(
-      body: ListaNoticias(headlines),
+      body: ( headlines.length == 0 )
+          ? Center( child: CircularProgressIndicator() )
+          : ListaNoticias(headlines)
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
+
+
 }
---
+++ lib/src/pages/tab2_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Tab2Page extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
