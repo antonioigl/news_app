298f13eb2665a9c4d93ffd5fe8d9dddef76e3aa9
Antonio <antonio241710@gmail.com>
2021-10-17 18:04:16 +0200

    added Puliendo el menú de categorias

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/src/models/category_models.dart';
 import 'package:news_app/src/services/news_service.dart';
 import 'package:provider/provider.dart';
 
@@ -31,17 +32,49 @@ class _ListaCategorias extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemCount: categories.length,
       itemBuilder: (BuildContext context, int index ) {
+
+        final cName = categories[index].name;
+
         return Padding(
           padding: EdgeInsets.all(8),
           child: Column(
             children: <Widget>[
-              Icon(categories[index].icon),
+              _CategoryButton(categories[index]),
               SizedBox(height: 5),
-              Text(categories[index].name)
+              Text( '${ cName[0].toUpperCase() }${cName.substring(1)}' )
             ],
           ),
         );
       },
+    );
+  }
+}
+
+class _CategoryButton extends StatelessWidget {
+
+  final Category categoria;
+
+  const _CategoryButton(this.categoria);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('${ categoria.name }');
+      },
+      child: Container(
+        width: 40,
+        height: 40,
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white
+        ),
+        child: Icon(
+          categoria.icon,
+          color: Colors.black54,
+        ),
+      ),
     );
   }
 }
